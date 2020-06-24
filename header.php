db7bd711168fec4fda437e8f562f5ae0cb0c9dbe
Michael <michael13117764@gmail.com>
2020-06-24 18:15:40 -0400

    Pagina Principal Hecha

---
+++ header.php
@@ -0,0 +1,110 @@
+
+<div class="d-flex" id="content-wrapper">
+
+<!-- Sidebar -->
+<div id="sidebar-container" class="bg-bleu">
+    <div class="logo-title">
+    <img class="imagen d-inline-block" src="image/logo2.png" alt="">
+        <h4 class="text-white font-weight-bold d-inline-block mb-0"> Biblioteca</h4>
+    </div>
+
+    <div class="menu">
+        <a href="#" class="d-block text-white p-3 border-0"><i class="icon ion-md-home lead mr-2"></i>
+            Inicio</a>
+
+        <a href="#" class="d-block text-white p-3 border-0"><i class="icon ion-md-people lead mr-2"></i>
+            Usuarios</a>
+
+        <a href="#" class="d-block text-white p-3 border-0"><i class="icon ion-md-book lead mr-2"></i>
+            Catalogo</a>
+        <a href="#" class="d-block text-white p-3 border-0"> <i class="icon ion-md-settings lead mr-2"></i>
+            Reportes</a>
+    </div>
+</div>
+<!-- Fin sidebar -->
+
+<div class="w-100">
+
+ <!-- Navbar -->
+ <nav class="navbar navbar-expand-md navbar-white naverr border-bottom py-0">
+    <div class="container">
+
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <form class="form-inline position-relative d-inline-block my-2">
+          <input class="form-control" type="search" placeholder="Buscar" aria-label="Buscar">
+          <button class="btn position-absolute btn-search" type="submit"><i class="icon ion-md-search"></i></button>
+        </form>
+        <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+          <li class="nav-item dropdown">
+            <a class="nav-link text-white dropdown-toggle" href="#" id="navbarDropdown" role="button"
+              data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Usuario
+            </a>
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">Mi perfil</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="cerrar.php">Cerrar sesión</a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- Fin Navbar -->
+
+<!-- Page Content -->
+<div id="content" class="bg-gris w-100">
+
+      <section class="bg-white py-3">
+          <div class="container">
+              <div class="row">
+                  <div class="col-lg-9 col-md-8">
+                    <h1 class="font-weight-bold mb-0">Dashboard</h1>
+                    <p class="lead text-muted">Libros</p>
+                  </div>
+              </div>
+          </div>
+      </section>
+
+      <section class="bg-mix py-3">
+        <div class="container">
+            <div class="card rounded-0">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-lg-3 col-md-6 d-flex my-3">
+                            <div class="mx-auto">
+                                <h5 class="text-muted">Total Libros</h5>
+                                <h2 class="font-weight-bold">0</h2>
+                                <h6 class="text-success">Registrados</h6>
+                            </div>
+                        </div>
+                        
+
+                        <div class="col-lg-3 col-md-6 d-flex my-3">
+                            <div class="mx-auto">
+                                <h5 class="text-muted">Total Descargas</h5>
+                                <h2 class="font-weight-bold">0</h2>
+                                <h6 class="text-success">Registrados</h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </section>
+
+ 
+</div>
+
+</div>
+</div>
+    <!-- Page Content -->
+    <div class="container">
+        <h1 class="mt-4"></h1>
+        <p></p>
+    </div>
+    <!-- /.container -->
---
+++ inicio.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="./css/main.css">
+    <title></title>
+    <link rel="stylesheet" href="./librerias/bootstrap4/bootstrap.min.css">
+    <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
+    
+    <!-- Google fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Muli:300,700&display=swap" rel="stylesheet">
+
+    <!-- Ionic icons -->
+    <link href="https://unpkg.com/ionicons@4.5.10-0/dist/css/ionicons.min.css" rel="stylesheet">
+
+
+</head>
+<body>
+
+
+<?php include "header.php"; ?>
+
+</section>
+		<!-- Content page -->
+		<div class="container-fluid">
+			<div class="page-header">
+			  <h1 class="text-center m-3">Libros</h1>
+			</div>
+		</div>
+		<div class="full-box text-center" style="padding: 50px 20px;">
+			<article class="full-box tile">
+				<div class="full-box tile-title text-center text-titles text-capitalize">Total</div>
+				<div class="full-box tile-icon text-center"><i class="zmdi zmdi-account"></i></div>
+				<div class="full-box tile-number text-titles">
+                    <p class="full-box">0</p><small>Registrados</small>
+				</div>
+            </article>
+            
+			<article class="full-box tile">
+				<div class="full-box tile-title text-center text-titles text-capitalize">Descargados</div>
+				<div class="full-box tile-icon text-center"><i class="zmdi zmdi-male-alt"></i></div>
+				<div class="full-box tile-number text-titles">
+					<p class="full-box">0</p><small>Registrados</small>
+				</div>
+            </article>
+
+
+		</div>
+	</section>
+
+
+<?php include "footer.php"; ?>
+
+</body>
+</html>
